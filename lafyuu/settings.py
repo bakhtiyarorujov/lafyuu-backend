76bf9a3df00014c1f8bf7a21b40f427dc3449190
bakhtiyarorujov <bakhtiyarorujov@gmail.com>
2023-12-04 18:49:13 +0400

    edit

--- lafyuu/settings.py
+++ lafyuu/settings.py
@@ -81,12 +81,8 @@ WSGI_APPLICATION = 'lafyuu.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'defaultdb',
-        'USER': 'doadmin',
-        'PASSWORD': 'AVNS_Ax98yD8vzgigBS-dB_j',
-        'HOST': 'postgres',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
